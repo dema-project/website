e39a3616695b319b8ca2d9bedc460c113379679d
Silas Jimmy <jimmysilas17@gmail.com>
2022-09-22 15:23:06 +0300

    feat: Create home page wireframes

--- demawebsite/src/main.js
+++ demawebsite/src/main.js
@@ -23,6 +23,9 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Core Styling */
+import './theme/core.css';
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
--- demawebsite/src/views/HomePage.vue
+++ demawebsite/src/views/HomePage.vue
@@ -1,68 +1,123 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header :translucent="true" class="ion-no-border b">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title size="large">Title</ion-title>
+
+        <ion-buttons slot="end" :collapse="true">
+          <ion-button class="ion-text-capitalize">Button</ion-button>
+          <ion-button class="ion-text-capitalize">Button</ion-button>
+          <ion-button
+            shape="round"
+            class="ion-text-capitalize"
+            color="primary"
+            fill="solid"
+            >Button</ion-button
+          >
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+      <ion-grid class="b ion-padding">
+        <ion-row class="b ion-align-items-end">
+          <ion-col class="b ion-text-center" size="6">
+            <h1>Title</h1>
+            <p>Subtitle</p>
+            <ion-button shape="round" class="ion-text-capitalize custom-btn"
+              >Button</ion-button
+            >
+          </ion-col>
+          <ion-col class="b" size="6">
+            <img class="b home-img" />
+          </ion-col>
+        </ion-row>
+
+        <ion-row class="b ion-text-center">
+          <ion-col size="12" class="b">
+            <ion-title>
+              <ion-text color="primary" class="ion-text-underline"
+                >Subtitle</ion-text
+              >
+            </ion-title>
+          </ion-col>
+          <ion-col size="4" class="b">
+            <img class="b home-img" />
+            <p>Some text</p>
+          </ion-col>
+          <ion-col size="4" class="b">
+            <img class="b home-img" />
+            <p>Some text</p>
+          </ion-col>
+          <ion-col size="4" class="b">
+            <img class="b home-img" />
+            <p>Some text</p>
+          </ion-col>
+        </ion-row>
+
+        <ion-row class="b ion-text-center">
+          <ion-col size="12" class="ion-text-center">
+            <img class="b banner-img" />
+          </ion-col>
+          <ion-col size="12" class="ion-text-center">
+            <div class="b intro-video"></div>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-footer>
+        <div class="b">
+          <ion-grid>
+            <ion-row>
+              <ion-col size="6">
+                <h1>Some stuff</h1>
+              </ion-col>
+              <ion-col size="6">
+                <h1>Some stuff</h1>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </div>
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-buttons>
+            <ion-button class="ion-text-capitalize">About</ion-button>
+            <ion-button class="ion-text-capitalize">Contact</ion-button>
+          </ion-buttons>
         </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      </ion-footer>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/vue";
 
-export default defineComponent({
-  name: 'HomePage',
+export default {
+  name: "HomePage",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
-});
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonGrid,
+    IonRow,
+    IonCol,
+  },
+};
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
+<style scoped></style>
