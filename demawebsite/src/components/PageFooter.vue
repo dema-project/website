95cfd8e65101f5f5d87888674198693d40dd29ae
Silas Jimmy <jimmysilas17@gmail.com>
2022-09-27 17:40:55 +0300

    feat: Implement the website footer

--- demawebsite/src/components/PageFooter.vue
+++ demawebsite/src/components/PageFooter.vue
@@ -1,13 +1,36 @@
 <template>
-  <ion-footer class="b">
-    <div class="ion-text-center">
+  <ion-footer>
+    <div class="ion-text-center footer-bg">
       <ion-grid>
-        <ion-row>
+        <ion-row class="ion-align-items-center ion-padding-vertical">
           <ion-col size="6">
-            <h1>Some stuff</h1>
+            <h2 class="font-weight-bold">
+              <ion-text color="light">Ready to get started?</ion-text>
+            </h2>
           </ion-col>
           <ion-col size="6">
-            <h1>Some stuff</h1>
+            <ion-item lines="none" color="transparent">
+              <ion-icon slot="start" :icon="person"></ion-icon>
+              <ion-label position="floating">Name</ion-label>
+              <ion-input clear-input type="text"></ion-input>
+            </ion-item>
+            <ion-item
+              lines="none"
+              color="transparent"
+              class="ion-margin-vertical"
+            >
+              <ion-icon slot="start" :icon="mail"></ion-icon>
+              <ion-label position="floating">Email</ion-label>
+              <ion-input clear-input type="email"></ion-input>
+            </ion-item>
+            <ion-button
+              shape="round"
+              expand="block"
+              color="primary"
+              class="ion-text-capitalize"
+            >
+              Download app
+            </ion-button>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -34,11 +57,23 @@ import {
   IonCol,
   IonToolbar,
   IonButtons,
+  IonInput,
+  IonItem,
+  IonLabel,
   IonButton,
+  IonIcon,
+  IonText,
 } from "@ionic/vue";
+import { person, mail } from "ionicons/icons";
 
 export default {
   name: "PageFooter",
+  data() {
+    return {
+      person,
+      mail,
+    };
+  },
   components: {
     IonFooter,
     IonGrid,
@@ -46,7 +81,12 @@ export default {
     IonCol,
     IonToolbar,
     IonButtons,
+    IonInput,
+    IonItem,
+    IonLabel,
     IonButton,
+    IonIcon,
+    IonText,
   },
 };
 </script>
